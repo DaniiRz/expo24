de5fa5ec57b910d454c95a5cf2d2babc286e0c2a
Daniela <20220434@ricaldone.edu.sv>
2024-04-11 21:34:03 -0600

    creacion de inicio de sesion y se arreglaron detalles

--- controllers/utils/plantillas.js
+++ controllers/utils/plantillas.js
@@ -3,7 +3,7 @@ function generarMenuIndex() {
 const menuContent = `
 
 <nav class="navbar navbar-expand-lg navbar-dark">
-    <a class="navbar-brand" href="#"><img src="../../resources/img/logo_header.png" alt="Logo"></a>
+    <a class="navbar-brand" href="../admin/inicio.html"><img src="../../resources/img/logo_header.png" alt="Logo"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
