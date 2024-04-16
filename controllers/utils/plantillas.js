f93423383e5d36208c9c20a6834d9a35ae03f74b
hwrnandez <107485692+Feraleck2005@users.noreply.github.com>
2024-04-16 12:12:33 -0600

    Cambios Andrès

--- controllers/utils/plantillas.js
+++ controllers/utils/plantillas.js
@@ -4,10 +4,6 @@ function generarMenuIndex() {
 
 <nav class="navbar navbar-expand-lg navbar-dark">
     <a class="navbar-brand" href="../admin/inicio.html"><img src="../../resources/img/logo_header.png" alt="Logo"></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
 </nav>
 
 <div class="sidebar mt-5">
