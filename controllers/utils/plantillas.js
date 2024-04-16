c4fc29506db06e3fa1c4aaba17df6edee066d41a
Daniela <20220406@ricaldone.edu.sv>
2024-04-16 09:22:21 -0600

    se actualizo sidebar

--- controllers/utils/plantillas.js
+++ controllers/utils/plantillas.js
@@ -10,50 +10,55 @@ function generarMenuIndex() {
     </button>
 </nav>
 
- <div class="sidebar mt-5">
- <a href="../admin/inicio.html" class="active"><img src="../../resources/img/hogar.png" alt=""><span
-         class="sidebar-text mx-3">Inicio</span></a>
- <a href="../admin/admin_estudiantes.html" class="mt-2"><img src="../../resources/img/alumno.png" alt=""><span
-         class="sidebar-text mx-3">Alumnos
-         ITR</span></a>
- <a href="../admin/admin_profesores.html" class="mt-2"><img src="../../resources/img/usuario-de-pizarra.png"
-         alt=""><span
-         class="sidebar-text mx-3">Profesores ITR</span></a>
- <a href="../admin/admin_administradores.html" class="mt-2"><img
-         src="../../resources/img/alt-administrador.png" alt=""><span
-         class="sidebar-text mx-3">Administradores
-         ITR</span></a>
- <a href="#" data-toggle="collapse" data-target="#subopciones" class="mt-2"><img src="../../resources/img/carpeta-abierta (2).png"
-         alt=""><span class="sidebar-text mx-3">Gestionar Formularios ↓</span></a>
- <div id="subopciones" class="collapse">
-     <ul>
-         <li><a href="../../views/admin/admin_roles_ptc.html">Roles PTC</a></li>
-         <li><a href="../../views/admin/admin_tipo_propuesta_ptc.html">Tipos de Propuestas</a></li>
-         <li><a href="../../views/admin/admin_estado_propuesta_ptc.html">Estados de Propuestas</a></li>
-         <li><a href="../../views/admin/admin_rubros_comerciales_ptc.html">Rubros Comerciales</a></li>
-     </ul>
- </div>
- <a href="#" data-toggle="collapse" data-target="#subopciones2" class="mt-2"><img src="../../resources/img/caracteristicas.png"
-         alt=""><span class="sidebar-text mx-3">Proyectos PTC ↓</span></a>
- <div id="subopciones2" class="collapse">
-     <ul>
-         <li><a href="#">Crear un Proyecto</a></li>
-         <li><a href="#">Proyectos Existentes</a></li>
-     </ul>
- </div>
- <a href="../../views/admin/admin_cursos.html" data-toggle="collapse" data-target="#subopciones3" class="mt-2"><img src="../../resources/img/leccion (1).png"
-         alt=""><span class="sidebar-text mx-3">Gestionar Cursos ↓</span></a>
- <div id="subopciones3" class="collapse">
-     <ul>
-         <li><a href="../../views/admin/admin_secciones.html">Secciones</a></li>
-         <li><a href="#">Especialidad</a></li>
-         <li><a href="#">Grupos</a></li>
-         <li><a href="#">Grados</a></li>
-     </ul>
- </div>
- <a href="../admin/index.html" class="mt-2"><img src="../../resources/img/salida.png" alt=""><span
-         class="sidebar-text mx-3">Cerrar
-         Sesión</span></a>
+<div class="sidebar mt-5">
+<a href="../admin/inicio.html" class="active"><img src="../../resources/img/hogar.png" alt=""><span
+        class="sidebar-text mx-3">Inicio</span></a>
+<a href="../../views/admin/admin_cursos.html" data-toggle="collapse" data-target="#subopciones3" class="mt-2"><img
+        src="../../resources/img/leccion (1).png" alt=""><span class="sidebar-text mx-3">Usuarios
+        ↓</span></a>
+<div id="subopciones3" class="collapse">
+    <ul>
+        <li><a href="../../views/admin/admin_estudiantes.html">Alumnos ITR</a></li>
+        <li><a href="../../views/admin/admin_profesores.html">Profesores ITR</a></li>
+        <li><a href="../../views/admin/admin_administradores.html">Administradores ITR</a></li>
+    </ul>
+</div>
+<a href="#" data-toggle="collapse" data-target="#subopciones" class="mt-2"><img
+        src="../../resources/img/carpeta-abierta (2).png" alt=""><span class="sidebar-text mx-3">Gestionar
+        Formularios ↓</span></a>
+<div id="subopciones" class="collapse">
+    <ul>
+        <li><a href="../../views/admin/admin_roles_ptc.html">Roles PTC</a></li>
+        <li><a href="../../views/admin/admin_tipo_propuesta_ptc.html">Tipos de Propuestas</a></li>
+        <li><a href="../../views/admin/admin_estado_propuesta_ptc.html">Estados de Propuestas</a></li>
+        <li><a href="../../views/admin/admin_rubros_comerciales_ptc.html">Rubros Comerciales</a></li>
+    </ul>
+</div>
+<a href="#" data-toggle="collapse" data-target="#subopciones2" class="mt-2"><img
+        src="../../resources/img/caracteristicas.png" alt=""><span class="sidebar-text mx-3">Proyectos PTC
+        ↓</span></a>
+<div id="subopciones2" class="collapse">
+    <ul>
+        <li><a href="#">Crear un Proyecto</a></li>
+        <li><a href="#">Proyectos Existentes</a></li>
+    </ul>
+</div>
+<a href="../../views/admin/admin_cursos.html" data-toggle="collapse" data-target="#subopciones3" class="mt-2"><img
+        src="../../resources/img/leccion (1).png" alt=""><span class="sidebar-text mx-3">Gestionar Cursos
+        ↓</span></a>
+<div id="subopciones3" class="collapse">
+    <ul>
+        <li><a href="../../views/admin/admin_secciones.html">Secciones</a></li>
+        <li><a href="#">Especialidad</a></li>
+        <li><a href="#">Grupos</a></li>
+        <li><a href="#">Grados</a></li>
+    </ul>
+</div>
+<a href="../../views/admin/admin_perfil.html" class="mt-2"><img src="../../resources/img/usuario.png" alt=""><span
+        class="sidebar-text mx-3">Perfil</span></a>
+<a href="../admin/index.html" class="mt-2"><img src="../../resources/img/salida.png" alt=""><span
+        class="sidebar-text mx-3">Cerrar
+        Sesión</span></a>
 </div>`;
 
         return menuContent;
