f080a718e9200159c6c29b571768e068b0e67ae6
L00MYY <160758553+L00MYY@users.noreply.github.com>
2024-04-22 07:52:56 -0600

    Se elimino tipo propuestas y rubros comerciales

--- controllers/utils/plantillas.js
+++ controllers/utils/plantillas.js
@@ -34,7 +34,6 @@ function generarMenuIndex() {
 <div id="subopciones" class="collapse">
     <ul>
         <li><a href="../../views/admin/admin_roles_ptc.html">Roles PTC</a></li>
-        <li><a href="../../views/admin/admin_tipo_propuesta_ptc.html">Tipos de Propuestas</a></li>
         <li><a href="../../views/admin/admin_estado_propuesta_ptc.html">Estados de Propuestas</a></li>
         
     </ul>
