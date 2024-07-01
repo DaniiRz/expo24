38ff1c22ba5276c76500c3652059fff7863844aa
Daniela <20220434@ricaldone.edu.sv>
2024-07-01 17:21:15 -0600

    se agregaron metodos scrud en handler de alumnos

--- api/models/handler/alumnosHandler.php
+++ api/models/handler/alumnosHandler.php
@@ -57,13 +57,32 @@ class AlumnoHandler
         return Database::getRows($sql);
     }
 
-    //Metodo para leer 
+    //Metodo para leer un solo registro 
     public function readOne()
     {
-        $sql = 'SELECT id_admin, nombre_admin, apellido_admin, correo_admin FROM tb_admins
-                WHERE id_admin = ?';
+        $sql = 'SELECT id_alumno, nombre_alumno, carnet_alumno, correo_alumno FROM alumnos
+                WHERE id_alumno = ?';
         $params = array($this->id);
         return Database::getRow($sql, $params);
+    }
+
+    //Metodo para actualizar un registro de alumno 
+    public function updateRow()
+    {
+        $sql = 'UPDATE alumnos
+                SET nombre_alumno = ?, carnet_alumno = ?, correo_alumno = ?
+                WHERE id_alumno = ?';
+        $params = array($this->nombre, $this->carnet, $this->correo, $this->id);
+        return Database::executeRow($sql, $params);
+    }
+
+    //Metodo para eliminar un registro de la tabla alumnos 
+    public function deleteRow()
+    {
+        $sql = 'DELETE FROM alumnos
+                WHERE id_alumno = ?';
+        $params = array($this->id);
+        return Database::executeRow($sql, $params);
     }
 
 }
